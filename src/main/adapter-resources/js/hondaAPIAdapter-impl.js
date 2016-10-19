025b046d5002d2fd1437d775c7c726fa83ff7723
nelsonkd <kenneth.nelson@cox.net>
2016-10-19 09:37:23 -0700

    Updated adapter to call API Connect Endpoints
    Updated adapter to call API Connect Endpoints

--- src/main/adapter-resources/js/hondaAPIAdapter-impl.js
+++ src/main/adapter-resources/js/hondaAPIAdapter-impl.js
@@ -1,39 +1,94 @@
-/*
- *  Licensed Materials - Property of IBM
- *  5725-I43 (C) Copyright IBM Corp. 2011, 2016. All Rights Reserved.
- *  US Government Users Restricted Rights - Use, duplication or
- *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
- */
+var CLIENT_ID = 'cb33657e-7328-45a1-b579-b87157400979';
+var ACCEPT = 'application/json';
+var JSON_CONTENT_TYPE = 'application/json';
+var URLENCODED_CONTENT_TYPE = 'application/x-www-form-urlencoded';
+var JSON = 'json';
+var POST = 'post';
+var GET = 'get';
 
-/**
- * @param tag: a topic such as MobileFirst_Platform, Bluemix, Cordova.
- * @returns json list of items.
- */
+var baseEndpoint;
+var ownersEndpoint;
+var vehiclesEndpoint;
+var componentsEndpoint;
+var updateEndpoint;
 
-function getFeed(tag) {
+
+var INPUT = {
+	method : this.GET,
+	returnedContentType : JSON,
+	headers: {
+		'Accept': ACCEPT,
+		'Content-Type': JSON_CONTENT_TYPE,
+		'x-ibm-client-id': CLIENT_ID
+	}
+};
+
+function getEndpoints(){
+	baseEndpoint = MFP.Server.getPropertyValue("baseEndpoint");
+	ownersEndpoint = MFP.Server.getPropertyValue("ownersEndpoint");
+	vehiclesEndpoint = MFP.Server.getPropertyValue("vehiclesEndpoint");
+	componentsEndpoint = MFP.Server.getPropertyValue("componentsEndpoint");
+	updateEndpoint  = MFP.Server.getPropertyValue("updateEndpoint");
+}
+
+function getOwner(ownerid) {
+	getEndpoints();
+
+	INPUT.path = baseEndpoint + ownersEndpoint + ownerid;
+	INPUT.method = GET;
+	return MFP.Server.invokeHttp(INPUT);
+}
+
+function getOwnerVehicles(ownerid){
+	getEndpoints();
+
+	INPUT.path = baseEndpoint + ownersEndpoint + ownerid + "/" + vehiclesEndpoint;
+	INPUT.method = GET;
+	return MFP.Server.invokeHttp(INPUT);
+}
+
+function getVehicle(vehicleid){
+	getEndpoints();
+
+	INPUT.path = baseEndpoint + vehiclesEndpoint + vehicleid;
+	INPUT.method = GET;
+	return MFP.Server.invokeHttp(INPUT);
+}
+
+function getVehicleComponents(vehicleid){
+	getEndpoints();
+
+	INPUT.path = baseEndpoint + vehiclesEndpoint + vehicleid + componentsEndpoint;
+	INPUT.method = GET;
+	return MFP.Server.invokeHttp(INPUT);
+}
+
+//where={"_id":"54321"}
+
+function setVehicleComponentStatus(componentID, status){
+	getEndpoints();
+
+	var dt = new Date();
+
+	var component =  "componentStatus=" + status;
+	var where = encodeURIComponent('{"_id": "' + componentID + '"}')
+
+
 	var input = {
-	    method : 'get',
-	    returnedContentType : 'xml',
-	    path : getPath(tag)
+		method : POST,
+		path: baseEndpoint + componentsEndpoint + updateEndpoint + '?where=' + where,
+		headers: {
+			'Accept' : JSON_CONTENT_TYPE,
+			'x-ibm-client-id': CLIENT_ID
+		},
+		body: {
+			content: component,
+			contentType: URLENCODED_CONTENT_TYPE
+		}
 	};
 
+	MFP.Logger.info("==> Debug");
+	MFP.Logger.info(input.path);
+
 	return MFP.Server.invokeHttp(input);
-}
-
-/**
- * Helper function to build the URL path.
- */
-function getPath(tag){
-    if(tag === undefined || tag === ''){
-        return 'feed.xml';
-    } else {
-        return 'blog/atom/' + tag + '.xml';
-    }
-}
-
-/**
- * @returns ok
- */
-function unprotected(param) {
-	return {result : "Hello from unprotected resource"};
 }
